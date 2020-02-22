02b61e2d2cc4dab1400385883d932f442b87bd55
Julien Avert <avert.julien@gmail.com>
2020-02-22 13:59:40 +0100

    fix linting...

--- src/createConnection.ts
+++ src/createConnection.ts
@@ -1,9 +1,15 @@
-import { HubConnectionBuilder, IHttpConnectionOptions, HubConnection } from '@aspnet/signalr';
+import {
+  HubConnectionBuilder,
+  IHttpConnectionOptions,
+  HubConnection,
+} from '@aspnet/signalr';
 
 /**
  * Creates a signalr hub connection.
  * @param url The URL of the signalr hub endpoint to connect to.
  * @param options Options object to pass to connection builder.
  */
-export const createConnection = (url: string, options: IHttpConnectionOptions = {}): HubConnection =>
-  new HubConnectionBuilder().withUrl(url, options).build();
+export const createConnection = (
+  url: string,
+  options: IHttpConnectionOptions = {}
+): HubConnection => new HubConnectionBuilder().withUrl(url, options).build();
