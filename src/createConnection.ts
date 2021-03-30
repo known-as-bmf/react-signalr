554dd81110e8687b0638da80a3bbcce7463b165b
edwardmcleodjones <55233094+edwardmcleodjones@users.noreply.github.com>
2021-03-30 11:19:38 +0100

    Add logging and reconnect options to createConnection
    `HubConnectionBuilder` can take additional chained calls to add logging and automatic reconnect options: https://docs.microsoft.com/en-us/javascript/api/@microsoft/signalr/hubconnectionbuilder?view=signalr-js-latest
    
    This change proposes adding two further optional parameters to the createConnection function to allow these to be passed in where required.

--- src/createConnection.ts
+++ src/createConnection.ts
@@ -2,14 +2,41 @@ import {
   HubConnectionBuilder,
   IHttpConnectionOptions,
   HubConnection,
+  LogLevel,
+  ILogger,
+  IRetryPolicy,
 } from '@microsoft/signalr';
 
 /**
  * Creates a signalr hub connection.
  * @param url - The URL of the signalr hub endpoint to connect to.
- * @param options - Options object to pass to connection builder.
+ * @param options - Optional options object to pass to connection builder.
+ * @param logging - Optional logging options to pass to connection builder.
+ * @param reconnectOptions - Optional reconnect options to pass to connection builder.
  */
 export const createConnection = (
   url: string,
-  options: IHttpConnectionOptions = {}
-): HubConnection => new HubConnectionBuilder().withUrl(url, options).build();
+  options: IHttpConnectionOptions = {},
+  logging: LogLevel | string | ILogger,
+  reconnectOptions: boolean | IRetryPolicy | number[]
+): HubConnection => {
+  const hubConnectionBuilder = new HubConnectionBuilder().withUrl(url, options);
+
+  if (logging) {
+    hubConnectionBuilder.configureLogging(logging);
+  }
+
+  if (reconnectOptions) {
+    if (typeof reconnectOptions === 'boolean') {
+      hubConnectionBuilder.withAutomaticReconnect();
+    } else if (Array.isArray(reconnectOptions)) {
+      hubConnectionBuilder.withAutomaticReconnect(reconnectOptions as number[]);
+    } else {
+      hubConnectionBuilder.withAutomaticReconnect(
+        reconnectOptions as IRetryPolicy
+      );
+    }
+  }
+
+  return hubConnectionBuilder.build();
+};
