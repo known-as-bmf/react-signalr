4d59d57cc8189e7e846859e7b31f840712d48eb0
Julien Avert <bmf.geekinside@gmail.com>
2021-04-12 10:47:11 +0200

    Add delegate to `createConnection` to further customize the `HubConnection`

--- src/createConnection.ts
+++ src/createConnection.ts
@@ -4,12 +4,19 @@ import {
   HubConnection,
 } from '@microsoft/signalr';
 
+export type HubConnectionBuilderDelegate = (
+  builder: HubConnectionBuilder
+) => HubConnectionBuilder;
+
 /**
  * Creates a signalr hub connection.
  * @param url - The URL of the signalr hub endpoint to connect to.
  * @param options - Options object to pass to connection builder.
+ * @param delegate - A delegate to further customize the HubConnection.
  */
 export const createConnection = (
   url: string,
-  options: IHttpConnectionOptions = {}
-): HubConnection => new HubConnectionBuilder().withUrl(url, options).build();
+  options: IHttpConnectionOptions = {},
+  delegate: HubConnectionBuilderDelegate = b => b
+): HubConnection =>
+  delegate(new HubConnectionBuilder()).withUrl(url, options).build();
